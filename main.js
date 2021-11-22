a6be0982491475eef27dcac0cfbf4350d3145ad3
Tahar-BRAHAM <brahamtahar97@gmail.com>
2021-11-22 09:50:59 +0100

    done

--- main.js
+++ main.js
@@ -45,7 +45,12 @@ function map(array, func) {
 // uppercaseAll(strArr); ==> [ 'HELLO', 'WORLD', 'WHIRLED', 'PEAS' ]
 
 function uppercaseAll(arrayOfStrings) {
-  // TODO: your code here
+// we call map with the return statement, for each element of our arrayOfStrings
+  return map(arrayOfStrings, function(string, i) {
+// ==> return all the element to upper case 
+// so as outupt it will be a new array with all the strings in capital letter
+    return string.toUpperCase()
+  });
 
 }
 
@@ -78,7 +83,11 @@ var data = [
 // Note: please write one or two lines here describing your solution.
 
 function highestPopulation(arrayOfObjects) {
-  // TODO: your code here
+  // so we return our filter function as usual and the condition of the output is 
+  // for each element of the array arrayOfObject, return only the element with the property population greater than 500 million 
+  return filter(arrayOfObjects, function(elem) {
+    return elem.population > 500000000 ; 
+  }) ; 
 
 }
 
@@ -93,7 +102,11 @@ function highestPopulation(arrayOfObjects) {
 // Note: please write one or two lines here describing your solution.
 
 function halveAll(numbers) {
-  // your code is here
+  // so we call map with the return statment, then the output will be a new array
+  // with all the element of the array, divided by 2 
+  return map(numbers, function(num) {
+    return num / 2 ;
+  })
 
 }
 
@@ -105,8 +118,14 @@ function halveAll(numbers) {
 // Note: please write one or two lines here describing your solution.
 
 function values(obj) {
-  // your code is here
-
+  //so here we used each to check if our input is an array or an object
+  //in this case its an object, then we pushed the propertys/values of our object inside a new array 
+  //and we return it 
+  var arrRes = [] ;
+  each(obj, function(element){
+    arrRes.push(element) ; 
+  })
+  return arrRes ; 
 }
 
 
